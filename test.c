7a15553f9469b9eb473f9b9d64e7aa038efaaa95
QWERTYPIG <luray0601@gmail.com>
2025-03-16 13:44:01 +0800

    added comments

--- test.c
+++ test.c
@@ -1,5 +1,5 @@
 #include<stdio.h>
 int main(){
-    printf("Hello World!\n");
-    return 0;
+    printf("Hello World!\n");//outputs welcoming message to terminal screen.
+    return 0;//returns normal exit status
 }
