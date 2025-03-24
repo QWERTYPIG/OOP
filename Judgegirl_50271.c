7d972f3e4c77de0e490beb6da26590e3a61df563
QWERTYPIG <luray0601@gmail.com>
2025-03-25 00:08:39 +0800

    add function for checking and updating previous variable/function

--- Judgegirl_50271.c
+++ Judgegirl_50271.c
@@ -23,6 +23,16 @@ bool is_reserved(char target[]){
     }
     return found;
 }
+bool find_previous(char target[], var_func var_funcs[], int var_func_sz){
+    bool found = 0;
+    for(int i = 0; i < var_func_sz && !found; i++){
+        if(strncmp(target, var_funcs[i].name, 24) == 0){
+            var_funcs[i].frequency++;
+            found = 1;
+        }
+    }
+    return found;
+}
 int main(){
     char inp[32], c, res[12][8] = {"if", "else", "while", "for", "switch", "case", "break", "return", "void", "int", "float", "char"};
     int sz = 0, inplen = 0;
@@ -40,20 +50,11 @@ int main(){
                     inplen = 0;
                     continue;
                 }
-                bool found = 0;
-                for(int i = 0; i < sz && !found; i++){
-                    if(strncmp(inp, var_funcs[i].name, 24) == 0){
-                        var_funcs[i].frequency++;
-                        found = 1;
-                        inplen = 0;
-                    }
-                }
-                if(!found){
+                if(!find_previous(inp, var_funcs, sz)){
                     strncpy(var_funcs[sz].name, inp, 24);
                     var_funcs[sz].name[min(24, inplen)] = '\0';
                     var_funcs[sz].frequency = 1;
                     sz++;
-                    inplen = 0;
                 }
             }
             inplen = 0;
@@ -61,21 +62,14 @@ int main(){
     }
     if(inplen != 0 && (isalpha(inp[0]) || inp[0] == '_')){
         bool found = is_reserved(inp);
-        if(found)inplen = 0;
-        for(int i = 0; i < sz && !found; i++){
-            if(strncmp(inp, var_funcs[i].name, 24) == 0){
-                var_funcs[i].frequency++;
-                found = 1;
-                inplen = 0;
-            }
-        }
+        if(!found)found = find_previous(inp, var_funcs, sz);
         if(!found){
             strncpy(var_funcs[sz].name, inp, 24);
             var_funcs[sz].name[min(24, inplen)] = '\0';
             var_funcs[sz].frequency = 1;
             sz++;
-            inplen = 0;
         }
+        inplen = 0;
     }
     for(int i = 0; i < sz; i++)printf("%s %d\n", var_funcs[i].name, var_funcs[i].frequency);
     return 0;
